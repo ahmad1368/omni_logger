6c99c0d6ad66d7198466bc470ac88d23a32badfb
ahmad1368 <ahmad.salempoor@gmail.com>
2026-06-25 22:06:49 -0700

    Rename package to ahmad_omni_logger and update tests for final stable release v1.0.0

--- test/omni_logger_test.dart
+++ test/omni_logger_test.dart
@@ -1,6 +1,6 @@
+import 'package:ahmad_omni_logger/ahmad_omni_logger.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
-import 'package:omni_logger/omni_logger.dart';
 
 void main() {
   // این دستور ریشه فلاتر را در محیط تست فعال می‌کند تا خطای بایندینگ برطرف شود
